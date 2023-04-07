f3804138caf3d159962e36e9c788f5d3dc26ed68
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-07 10:04:53 +0530

    [Pankaj] Employee is Present or Not

---
+++ EmpWageCompProg/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageCompProg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 1;
+            //Welcome message
+            Console.WriteLine("Welcome to Employee Wage Comp Prob Statement");
+            //UC1-ifPresentOrNot
+            Random random = new Random();
+            int empAttendence = random.Next(0, 2);
+            if(empAttendence == 0) 
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
