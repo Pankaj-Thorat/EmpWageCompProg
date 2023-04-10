168d2940309edcae24ee9d80c6da87a9e93e4eaf
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-10 22:56:00 +0530

    [Panka] monthly wage till condition

--- EmpWageCompProg/Program.cs
+++ EmpWageCompProg/Program.cs
@@ -12,18 +12,20 @@ namespace EmpWageCompProg
         const int isFullTime = 2;
         const int numberOfWorkingDays = 20;
         const int empWageRate = 20;
-
+        const int maxHrsInMonth = 100;
 
         static void Main(string[] args)
         {
-            int empHrs = 0;
-            int empDailyWage = 0;
+            int totalEmpHrs = 0;
+            int empHrs = 0;   
             int totalEmpWage = 0;
+            int totalWorkingDays = 0;
             //Welcome message
             Console.WriteLine("Welcome to Employee Wage Comp Prob Statement");
-            //UC5-monthlyWage
-            for (int day = 1; day <= numberOfWorkingDays; day++)
+            //UC6-monthlyWageTillCondition
+            while (totalWorkingDays < numberOfWorkingDays && totalEmpHrs < maxHrsInMonth )
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empAttendence = random.Next(0, 3);
                 switch (empAttendence)
@@ -40,11 +42,12 @@ namespace EmpWageCompProg
                         empHrs = 0;
                         break;
                 }
-                empDailyWage = empHrs *empWageRate;
+                int empDailyWage = empHrs * empWageRate;
                 totalEmpWage += empDailyWage;
-                Console.WriteLine("Employee Daily wage for day{0} is: " + empDailyWage + " rs",day);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("total wage:" + totalEmpWage + "for "+ totalWorkingDays +" days and "+ totalEmpHrs +"hrs");
             }
-            Console.WriteLine("Employee Total wage for {0} days is {1} rs", numberOfWorkingDays, totalEmpWage);
+            Console.WriteLine("Employee Total wage for {0} days and {1}hrs is {2} rs", totalWorkingDays,totalEmpHrs, totalEmpWage);
         }
     }
 }
