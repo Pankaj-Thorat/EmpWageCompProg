a29a883f87ee9ade6c12e4a86e3c39050b027ccb
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-04-10 21:51:09 +0530

    [Pankaj] Monthly wage calculation

--- EmpWageCompProg/Program.cs
+++ EmpWageCompProg/Program.cs
@@ -10,35 +10,41 @@ namespace EmpWageCompProg
     {
         const int isPartTime = 1;
         const int isFullTime = 2;
+        const int numberOfWorkingDays = 20;
+        const int empWageRate = 20;
+
+
         static void Main(string[] args)
         {
-           
             int empHrs = 0;
-            const int empWageRate = 20;
             int empDailyWage = 0;
+            int totalEmpWage = 0;
             //Welcome message
             Console.WriteLine("Welcome to Employee Wage Comp Prob Statement");
-            //UC1-ifPresentOrNot
-            Random random = new Random();
-            int empAttendence = random.Next(0, 3);
-            switch (empAttendence) 
+            //UC5-monthlyWage
+            for (int day = 1; day <= numberOfWorkingDays; day++)
             {
-                case isFullTime:
-                    Console.WriteLine("Employee is FullTime ");
-                    empHrs = 8;
-                    break;
+                Random random = new Random();
+                int empAttendence = random.Next(0, 3);
+                switch (empAttendence)
+                {
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
 
-                case isPartTime:
-                    Console.WriteLine("Employee is PartTime ");
-                    empHrs = 4;
-                    break;
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
 
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empDailyWage = empHrs *empWageRate;
+                totalEmpWage += empDailyWage;
+                Console.WriteLine("Employee Daily wage for day{0} is: " + empDailyWage + " rs",day);
             }
-            empDailyWage = empHrs * empWageRate;
-            Console.WriteLine("Employee Daily wage is: "+ empDailyWage +" rs");
+            Console.WriteLine("Employee Total wage for {0} days is {1} rs", numberOfWorkingDays, totalEmpWage);
         }
     }
 }
